46c59cfc5d63bdb3d001d106ac566c16d17bc7c4
gshiyanov <gshiyanov@ozon.ru>
2022-05-02 15:03:28 +0300

    Moved to Solved Folder

--- Python solutions/1-10/Task_09.py
+++ Python solutions/1-10/Task_09.py
@@ -2,7 +2,7 @@
 A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
 
 a^2 + b^2 = c^2
-For example, 3^2 + 4^2 = 9 + 16 = 25 = 52.
+For example, 3^2 + 4^2 = 9 + 16 = 25 = 5^2.
 
 There exists exactly one Pythagorean triplet for which a + b + c = 1000.
 Find the product abc.
@@ -10,10 +10,11 @@ Find the product abc.
 Тройка Пифагора - три натуральных числа a < b < c, для которых выполняется равенство
 
 a^2 + b^2 = c^2
-Например, 3^2 + 4^2 = 9 + 16 = 25 = 52.
+Например, 3^2 + 4^2 = 9 + 16 = 25 = 5^2.
 
 Существует только одна тройка Пифагора, для которой a + b + c = 1000.
 Найдите произведение abc.
 '''
 
+target_sum = 1000
 
