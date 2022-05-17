14b7d947d6db67d1e41344f9f29b49394e12b806
gshiyanov <gshiyanov@ozon.ru>
2022-05-17 23:16:18 +0300

    test 1

---
+++ C# solutions/1-10/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Euler
+{
+    class Class1
+    {
+        public int taskid;
+        // Inheritance
+        public void GetTask(int id)
+        {
+            Console.WriteLine( "Task " + id );
+        }
+        public void SolveTask()
+        {
+            Console.WriteLine("Task 1 Solution");
+        }
+    }
+}
---
+++ C# solutions/Euler-CS/Program.cs
@@ -0,0 +1,22 @@
+﻿using Euler;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Euler
+{
+    class Program
+    {
+        static public void Main(string[] args)
+        {
+            Console.WriteLine("Hello !");
+            
+
+            
+            
+        }
+    }   // end of Profram
+}   // end of namespace Euler
